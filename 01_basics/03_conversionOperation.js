3ba5c3115f5a3683c758ca1aea47998e914e9a45
ahamadarbab <ahmadarbab.456@gmail.com>
2026-03-30 17:43:32 +0530

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+let score = "Rohan"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber); 
+
+// "33" => 33
+// "33abc" or "Rohan" => NaN
+// true => 1; false => 0
+
+
+let isLoggedIn = "";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Rohan" => true
+
+
+let someNumber = 45
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
