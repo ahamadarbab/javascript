59afb204adea4e5d9670aee19c1897fd4b6babdf
ahamadarbab <ahmadarbab.456@gmail.com>
2026-04-01 00:05:32 +0530

    Summary of Datatypes

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,43 @@
+// 1. Primitive
+
+// 7 types: Number, String, Boolean, BigInt, null, Symbol, undefined
+
+const score = 100   // Number
+const scoreValue = 100.3    // Number
+
+let name = "Ajay"   // String
+let city = "Jaipur"    // String
+
+const isLoggedIn = false    // Boolean
+const istrue = true    // Boolean
+
+const bigNumber = 343165133561353555113n    // BigInt
+
+const outsideTemp = null    // null
+
+const id = Symbol('123')    // Symbol
+const anotherId = Symbol('123')    // Symbol
+
+let userEmail;    // undefined
+let userPhone = undefined    // undefined
+
+
+// 2. Non primitive (Reference)
+
+// Array, Objects, Functions
+
+const superHeros = ["Shaktiman", "krish", "Hulk"];    // Array
+
+let myObj = {       
+    name: "Rohan",      // Object
+    age: 22,
+}
+
+const myFunction = function() {
+    console.log("Hello World");     // Function
+}
+myFunction();
+
+console.log(typeof bigNumber);
+
+
