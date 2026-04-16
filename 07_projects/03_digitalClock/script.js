219ad5212b13ccc7a91bbfc4993df78f2c12657f
ahamadarbab <ahmadarbab.456@gmail.com>
2026-04-17 02:04:07 +0530

    Project 5: Keyboard keyCode

--- 07_projects/03_digitalClock/script.js
+++ 07_projects/03_digitalClock/script.js
@@ -4,9 +4,12 @@ const clock = document.getElementById("clock")
 // let date = new Date();
 // console.log(date.toLocaleTimeString());
 
+const timeText = document.createTextNode("")
+clock.appendChild(timeText)
 
 setInterval(function(){
     let date = new Date();
     // console.log(date.toLocaleTimeString());
-    clock.innerHTML = date.toLocaleTimeString();
+    // clock.innerHTML = date.toLocaleTimeString();
+    timeText.nodeValue = date.toLocaleTimeString();
 }, 1000)
